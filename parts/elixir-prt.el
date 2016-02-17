43face140e21560550c5210218142cb435787349
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2016-02-17 15:03:01 -0200

    elixir windows

--- parts/elixir-prt.el
+++ parts/elixir-prt.el
@@ -1,6 +1,16 @@
 (use-package alchemist
   :config
-  (progn
-    (add-hook 'elixir-mode-hook 'alchemist-mode)))
+    (add-hook 'elixir-mode-hook 'alchemist-mode)
+    (add-to-list 'display-buffer-alist
+      `(,(rx bos (or "*alchemist test report*"
+                     "*alchemist mix*"
+                     "*alchemist help*"
+                     "*alchemist elixir*"
+                     "*alchemist elixirc*"))
+                 (display-buffer-reuse-window
+                  display-buffer-in-side-window)
+                 (reusable-frames . visible)
+                 (side            . right)
+                 (window-width   . 0.5))))
 
 (provide 'elixir-prt)
