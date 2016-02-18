46522c8728d206d676a80e0065938b7aace21f3e
Leandro Cesquini Pereira <leandro.cesquini@gmail.com>
2016-02-18 13:40:22 -0200

    yasnippets

--- parts/completion-prt.el
+++ parts/completion-prt.el
@@ -1,6 +1,6 @@
 (use-package company
   :init
-    (setq company-idle-delay 0)
+    (setq company-idle-delay 0.5)
     (setq company-tooltip-limit 10)
     (setq company-minimum-prefix-length 2)
     (setq company-tooltip-flip-when-above t)
@@ -9,10 +9,17 @@
     (add-hook 'prog-mode-hook 'company-mode)
     (bind-key "C-<tab>" 'company-complete)
     (bind-keys :map company-active-map
-               ("<tab>" . company-complete-common-or-cycle)
-               ("C-j"   . company-select-next)
-               ("C-k"   . company-select-previous))
+               ("TAB" . company-complete-common-or-cycle)
+               ("C-j" . company-select-next)
+               ("C-k" . company-select-previous))
+    (define-key company-active-map (kbd "TAB") 'company-complete-common-or-cycle)
   :bind
     ("<C-tab>" . company-complete))
+
+(use-package yasnippet
+  :config
+  (yas-global-mode 1))
+
+(use-package elixir-yasnippets)
 
 (provide 'completion-prt)
